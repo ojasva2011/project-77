f383866123942efa565842ecb885bcb67f76cf99
ojasva2011 <90315077+ojasva2011@users.noreply.github.com>
2022-06-19 19:41:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="Ranbir";
+console.log(first_name);
+var last_name="Kapoor";
+console.log(last_name);
+var full_name=first_name.concat(last_name)
+console.log(full_name)
+var a = 10;
+console.log(a)
+var b = 5;
+console.log(b)
